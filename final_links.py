192b5f74c0635a30ada4e2e20be85488949a6fd7
Vishnu-priyan <vishnu13126@cse.ssn.edu.in>
2017-10-28 15:43:34 +0530

    Added file to get final links!

---
+++ final_links.py
@@ -0,0 +1,33 @@
+#Getting from final_sub_page!
+#This outputs links such as
+"""
+---------------------------------------------------------------------
+http://www.icd10data.com//ICD10CM/Codes/V00-Y99/V10-V19/V10-/V10
+http://www.icd10data.com//ICD10CM/Codes/V00-Y99/V10-V19/V10-/V10.0
+http://www.icd10data.com//ICD10CM/Codes/V00-Y99/V10-V19/V10-/V10.0XXA
+http://www.icd10data.com//ICD10CM/Codes/V00-Y99/V10-V19/V10-/V10.0XXD
+http://www.icd10data.com//ICD10CM/Codes/V00-Y99/V10-V19/V10-/V10.0XXS
+http://www.icd10data.com//ICD10CM/Codes/V00-Y99/V10-V19/V10-/V10.1
+http://www.icd10data.com//ICD10CM/Codes/V00-Y99/V10-V19/V10-/V10.1XXA
+...(All such links)
+...
+Till last one!
+---------------------------------------------------------------------
+Where input link is as
+http://www.icd10data.com//ICD10CM/Codes/V00-Y99/V10-V19
+(Such as those obtained from reqfile.py)
+---------------------------------------------------------------------
+"""
+
+import requests
+import bs4
+from reqfile import print_in_order,main_link_finder
+url = "http://www.icd10data.com/ICD10CM/Codes/A00-B99/A15-A19"
+
+#texts_class = body-content | link_class = 'identifierSpacing identifier'
+
+texts,links = main_link_finder(url,'identifierSpacing identifier',"body-content")
+
+print_in_order(links)
+print("::::::::::::::::::::::::::")
+print_in_order(texts[:-2])
